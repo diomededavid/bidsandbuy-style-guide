61dc4b05b8d7b3c6d7e1c882bbfd732acb25afcd
diomededavid <david@d2creativedesign.com>
2023-11-05 18:36:29 -0700

    first commit

---
+++ src/js/main.js
@@ -0,0 +1,10 @@
+// Import our custom CSS
+import '../scss/styles.scss'
+
+import '../images/bids-and-buy-logo-horiz.svg';
+
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap'
+
+
+
