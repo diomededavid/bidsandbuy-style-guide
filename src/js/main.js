d259b02fc14b6d2c26408e506d5c24b9f713fb4a
diomededavid <david@d2creativedesign.com>
2023-11-05 18:41:21 -0700

    github pages only allows for docs folder

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,6 @@
 // Import our custom CSS
 import '../scss/styles.scss'
 
-import '../images/bids-and-buy-logo-horiz.svg';
-
 // Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap'
 
